722894d7ba19eca8853b20168710cc4d7306eb68
zainmujahid <zainmujahid01@gmail.com>
2020-11-11 23:40:06 +0500

    Maven project files created with spring initializer, spring boot version 2.3.5, JAVA 11, JAR, Spring Web, Thymeleaf

---
+++ src/test/java/com/word_counter/web_application/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.word_counter.web_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
